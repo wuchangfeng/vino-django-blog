904cb92f5dbe50156492d282a19c1336ef82da4e
wuchangfeng <wuchangfeng2015@gmail.com>
2016-11-04 16:57:37 +0800

    First push to GitHub on Mac

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import url
 from . import views
 from django.conf import settings
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^article/(?P<article_id>\d+)$', views.ArticleDetailView.as_view(), name='detail'),
     url(r'^category/(?P<category_id>\d+)$', views.CategoryView.as_view(), name='category'),
     url(r'^tag/(?P<tag_id>\d+)$', views.TagView.as_view(), name='tag'),
+    url(r'^favicon.ico$', RedirectView.as_view(url='/static/images/favicon.ico')),
+    #url(r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '/static/images/favicon.ico'}),
 ]
