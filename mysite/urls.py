e13acd01376f3715ed68921c5dd942dcfbadbc59
wuchangfeng <wuchangfeng2015@gmail.com>
2016-11-06 22:08:46 +0800

    add comment tab

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,25 +1,17 @@
-"""chenblog URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Add an import:  from blog import urls as blog_urls
-    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
-"""
 from django.conf.urls import include, url
 from django.contrib import admin
 from blog.views import IndexView as IndexView
+from blog.views import about_me as about_me
+from blog.views import archives as archives
+from blog.views import comments as comments
+
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
     url(r'^blog/', include('blog.urls', namespace='blog')),
-    url(r'^pages/', include('django.contrib.flatpages.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^about/', about_me, name='about_me'),
+    url(r'^archives/$', archives, name = 'archives'),
+    url(r'^comments/$', comments, name = 'comments'),
 ]
