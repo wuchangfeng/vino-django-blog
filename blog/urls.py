8fadf8e51b0ce58ec0d65d652cff07f1a9086b10
wuchangfeng <wuchangfeng2015@gmail.com>
2016-11-11 15:50:55 +0800

    add a favicon for add htmls

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.conf import settings
 from django.views.generic.base import RedirectView
 
-favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
+
 
 
 urlpatterns = [
@@ -11,6 +11,5 @@ urlpatterns = [
     url(r'^article/(?P<article_id>\d+)$', views.ArticleDetailView.as_view(), name='detail'),
     url(r'^category/(?P<category_id>\d+)$', views.CategoryView.as_view(), name='category'),
     url(r'^tag/(?P<tag_id>\d+)$', views.TagView.as_view(), name='tag'),
-    url(r'^favicon.ico$', favicon_view),
     url(r'^search/$', views.SearchView.as_view(), name='search'),
 ]
